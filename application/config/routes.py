62cff48984e97ee1575985c2fefe11a103284035
Your Name <you@example.com>
2021-02-12 22:26:02 +0700

    Fri Feb 12 22:26:02 WIB 2021

--- application/config/routes.py
+++ application/config/routes.py
@@ -4,7 +4,7 @@ from application.config.config import *
 from application.controller.contoh.controller import contoh
 
 app = Flask(__name__)
-flare_config(app)
+app = flare_config(app)
 app.register_blueprint(contoh,url_prefix='/contoh')
 
 if "__main__" == __name__:
