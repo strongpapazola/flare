e2ec4dfc96810da9c755952f774ef9b9ce1a42e8
M Bintang Cahya Putra <strongpapazola@gmail.com>
2021-02-26 10:45:11 +0700

    'up'

--- application/config/config.py
+++ application/config/config.py
@@ -1,14 +1,23 @@
 from application.config.autoload import *
-from flask_pymongo import PyMongo
 
-def main():
-    app = Flask(__name__)
-    app.config['MONGO_URI'] = 'mongodb://admin:admin@portal.bisaai.id:27017/test'
-    mongo = PyMongo(app)
-    return app, mongo
+mongo = PyMongo()
 
+def create_app(name):
+    app = Flask(name)
+    app.config['MONGO_URI'] = 'mongodb://admin:admin@portal.bisaai.id:27017/digileaps'
+    mongo.init_app(app)
+    return app
 
-def init_mongo(app):
-    return PyMongo(app)
+# class Config(object):
+#     def __init__(self, app=None, uri=None, *args, **kwargs):
+#         if app is not None:
+#             self.init_app(app, uri, *args, **kwargs)
+
+    # def init():
+    #     app = Flask(__name__)
+    #     app.config['MONGO_URI'] = 'mongodb://admin:admin@portal.bisaai.id:27017/test'
+
+    # def init_mongo():
+    #     self.init().app
 
 
--- application/config/routes.py
+++ application/config/routes.py
@@ -1,10 +1,11 @@
-from application.config.autoload import *
 from application.config.config import *
-
 from application.controller.contoh.controller import contoh
 
-app, null = main()
+app = create_app(__name__)
 app.register_blueprint(contoh,url_prefix='/contoh')
 
 if "__main__" == __name__:
-    app.run()
+    app.run(debug=True, port=9090)
+
+# if "__main__" == __name__:
+#     app.run()
